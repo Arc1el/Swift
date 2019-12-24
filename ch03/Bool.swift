7e397d3bd6c84ce3334b035b01940e2eb197bc6f
Arc1el <khm970514@gmail.com>
2019-12-24 17:09:01 +0900

    Create Bool.swift

---
+++ ch03/Bool.swift
@@ -0,0 +1,7 @@
+var boolean : Bool = true
+boolean.toggle() //invert true-false
+let iLoveYou : Bool = true
+let isTimeUnlimited : Bool = false
+
+print("the time is unlimited? : \(isTimeUnlimited)")
+
