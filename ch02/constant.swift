64bf96a01318fae201278574057e4539f5862e2d
Arc1el <khm970514@gmail.com>
2019-12-24 16:42:20 +0900

    Create constant.swift

---
+++ ch02/constant.swift
@@ -0,0 +1,11 @@
+let name : String = "hyeonmin"
+var age : Int = 23
+var job = "ios programmer" //타입추론사용
+let height = 179.8
+
+
+age = 23
+job = "Writer"
+
+
+print("저의 이름은 \(name)이고, 나이는 \(age)세이며, 직업은 \(job)입니다. 비밀이지만 키는 \(height)센티미터입니다.")
