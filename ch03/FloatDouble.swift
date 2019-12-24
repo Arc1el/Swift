fdc0d7d89b60e034fba06f530e925065dcb5446d
Arc1el <khm970514@gmail.com>
2019-12-24 17:18:08 +0900

    Create FloatDouble.swift

---
+++ ch03/FloatDouble.swift
@@ -0,0 +1,9 @@
+var floatValue : Float = 1234567890.1
+let doubleValue : Double = 1234567890.1
+
+print("floatValue : \(floatValue), DoubleValue : \(doubleValue)")
+
+floatValue = 123456.1
+
+print(floatValue)
+
