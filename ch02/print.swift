4f0782bd50abe86fbf115741b9f42f671ae5387a
Arc1el <khm970514@gmail.com>
2019-12-24 16:20:27 +0900

    Create print.swift

---
+++ ch02/print.swift
@@ -0,0 +1,25 @@
+import UIKit
+import SwiftUI
+
+struct BasicInformation
+{
+    let name: String
+    var age: Int
+}
+
+var hyeonminInfo : BasicInformation = BasicInformation(name : "hyeonmin", age : 23)
+
+class Person
+{
+    var height : Float = 0.0
+    var weight : Float = 0.0
+}
+let hyeonmin : Person = Person()
+hyeonmin.height = 179.8
+hyeonmin.weight = 82.1
+
+print(hyeonminInfo)
+dump(hyeonminInfo)
+
+print(hyeonmin)
+dump(hyeonmin)
