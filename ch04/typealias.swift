ad29107294cdfa3677e33991b16076d0398d8c85
Arc1el <khm970514@gmail.com>
2019-12-25 14:29:26 +0900

    Create typealias.swift

---
+++ ch04/typealias.swift
@@ -0,0 +1,11 @@
+typealias MyInt = Int
+typealias YourInt = Int
+typealias MyDouble = Double
+
+let age : MyInt = 100
+var year : YourInt = 2080
+
+year = age //두 데이터 타입 모두 Int형이기때문에 같은타입 취급
+
+let month : Int = 7 //기존 Int도 사용가능
+let percentage : MyDouble = 99.9
