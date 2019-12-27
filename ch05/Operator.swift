e423fd14613cb9cf0f3323d4c49c71120eca65b7
Arc1el <khm970514@gmail.com>
2019-12-27 16:59:33 +0900

    Update Operator.swift

--- ch05/Operator.swift
+++ ch05/Operator.swift
@@ -11,3 +11,16 @@ let referenceC : SomeClass = referenceA
 
 let isSameReferenceAB : Bool = referenceA === referenceB
 let isSameReferenceBC : Bool = referenceB === referenceC
+
+var valueA : Int = 3
+var valueB : Int = 5
+var biggerValue : Int = valueA > valueB ? valueA : valueB
+
+valueA = 0
+valueB = -3
+biggerValue = valueA > valueB ? valueA : valueB
+
+var stringA : String = ""
+var stringB : String = "String"
+var resultValue : Double = stringA.isEmpty ? 1.0 : 0.0
+resultValue = stringB.isEmpty ? 1.0 : 0.0
